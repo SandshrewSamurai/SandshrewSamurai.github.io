5f37d776797b20214fa096d043007522c3c3f09d
SandshrewSamurai <31966971+SandshrewSamurai@users.noreply.github.com>
2021-05-25 20:38:22 +0100

    Added Shinies of all pokemon
    Added a checkbox to make the pokemon shiny and hopefully got all shiny sprites working properly.

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let team = [];
+let shiny = [];
 let dropdownData = [];
 
 function load() {
@@ -19,6 +20,7 @@ function draw() {
 
 function updateTeam(){
     team = [];
+    shiny = [];
     for (let i = 0; i < 6; i++) {
         let pkmn = document.getElementById("pokemon-dropdown-" + i);
         dropdownData[i] = pkmn.options[pkmn.selectedIndex].value;
@@ -27,6 +29,11 @@ function updateTeam(){
             team.push("");
         } else {
             team.push(ID);
+        }
+        if (document.getElementById("shiny-" + i).checked){
+            shiny.push(true);
+        } else {
+            shiny.push(false);
         }
     }
     drawTeam(750, 450);
@@ -59,7 +66,7 @@ function drawTeam(w, h) {
 	//Load and Draw Required Pokemon
 	for (let i = 0; i < 6; i++) {
 		if (team[i] !== "") {
-			drawPokemon(x, y, size, team[i]);
+			drawPokemon(x, y, size, team[i], shiny[i]);
 
 			if (y == pad / 2){
 				y += size + pad;
@@ -71,15 +78,20 @@ function drawTeam(w, h) {
 	}
     
     //Loads and Displays Images
-    function drawPokemon(x, y, size, ID){
+    function drawPokemon(x, y, size, ID, isShiny){
 	//if (poke.exists) {
 		image(ballImg, x, y, size, size);
-		loadImage(getImage(ID), img => image(img, x, y, size, size));
+		loadImage(getImage(ID, isShiny), img => image(img, x, y, size, size));
 	//}
 
     }
 }
 
-function getImage(ID) {
-		return ("Sprites/" + (ID) + ".png");
+function getImage(ID, isShiny) {
+    if (isShiny) {
+        return ("Sprites/shiny/" + (ID) + ".png");
+    } else {
+		return ("Sprites/" + (ID) + ".png");        
+    }
+
 }
