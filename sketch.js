95af12130319fddf1d6c52d353c926c48ccc38a2
SandshrewSamurai <31966971+SandshrewSamurai@users.noreply.github.com>
2022-02-22 12:21:14 +0000

    Layout Options
    Added Layout options to team display

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 let teamData = [];
+let layout;
 
 function preload() {
     ballImg = loadImage("Pokeball.PNG");
     if (localStorage.getItem("teamData")) {
         teamData = JSON.parse(localStorage.getItem('teamData'));
+    }
+    if (localStorage.getItem("layout")) {
+        layout = localStorage.getItem('layout');
     }
 }
 
@@ -21,7 +25,21 @@ function draw() {
 //Handles All Visuals
 function drawTeam(w, h) {
 	//Calculate Spacing and Size of Images
-	let size = w / 4;
+	let size;
+    switch (layout){
+        case "h1":
+            size = w / 6.5;
+            break;
+        case "h2":
+            size = w / 3.5;
+            break;
+        case "v1":
+            size = h / 6.5;
+            break;
+        case "v2":
+            size = h / 3.5;
+            break;
+    }
 	let pad = 20;
 	let x = pad / 2;
 	let y = pad / 2;
@@ -31,14 +49,32 @@ function drawTeam(w, h) {
 	for (let i = 0; i < 6; i++) {
 		if (teamData[((i + 1) * 2) - 2] !== "") {
 			drawPokemon(x, y, size, teamData[((i + 1) * 2) - 2], teamData[((i + 1) * 2) - 1]);
-
-			if (y == pad / 2){
-				y += size + pad;
-			} else {
-				y = pad / 2;
-				x += size + pad;
-			}
-		}
+            
+            switch (layout){
+                case "h1":
+                    x += size + pad;
+                    break;
+                case "h2":
+                    if (y == pad / 2){
+                        y += size + pad;
+                    }else {
+                        y = pad / 2;
+                        x += size + pad;
+                    }
+                    break;
+                case "v1":
+                    y += size + pad;
+                    break;
+                case "v2":
+                    if (x == pad / 2){
+                        x += size + pad;
+                    }else {
+                        x = pad / 2;
+                        y += size + pad;
+                    }
+                    break;
+            }
+        }
     }
     
     //Listen for team change
@@ -47,8 +83,8 @@ function drawTeam(w, h) {
         drawTeam(windowWidth * 0.9, windowHeight * 0.9);
     };
 }
-    
-    //Loads and Displays Images
+
+//Loads and Displays Images
 function drawPokemon(x, y, size, ID, isShiny){
 	image(ballImg, x, y, size, size);
 	loadImage(getImage(ID, isShiny), img => image(img, x, y, size, size));
